bab0d3ce34b62e23b39a813df0561b531c693b19
dinoslebew <awdino966@gmail.com>
2025-03-19 15:23:53 +0700

    update masuk view dan dafar view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/masuk', function () {
+    return view('masuk');
+});
+
+Route::get('/daftar', function () {
+    return view('daftar');
 });
